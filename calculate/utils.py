777e6d3058f0525cceaddd549b9b0d6685bf2679
Abhijit Gupta <avgupta456@gmail.com>
2020-05-30 15:22:18 -0400

    updated utils

--- calculate/utils.py
+++ calculate/utils.py
@@ -1,4 +1,6 @@
 import pickle
+import shutil
+import os
 
 def dump(path, data):
     with open(path, 'wb') as f:
@@ -9,32 +11,32 @@ def load(file):
         return pickle.load(f)
 
 def saveMatches(year, matches):
-    dump("./data/matches/matches_"+str(year)+".p", matches)
+    dump("data/matches/matches_"+str(year)+".p", matches)
 
 def loadMatches(year):
-    return load("./data/matches/matches_"+str(year)+".p")
+    return load("data/matches/matches_"+str(year)+".p")
 
 def saveProcessedMatches(year, matches):
-    dump("./data/processed_matches/matches_"+str(year)+".p", matches)
+    dump("data/processed_matches/matches_"+str(year)+".p", matches)
 
 def loadProcessedMatches(year):
-    return load("./data/processed_matches/matches_"+str(year)+".p")
+    return load("data/processed_matches/matches_"+str(year)+".p")
 
 def saveTeams(year, teams):
-    dump("./data/teams/teams_"+str(year)+".p", teams)
+    dump("data/teams/teams_"+str(year)+".p", teams)
 
 def loadTeams(year):
-    return load("./data/teams/teams_"+str(year)+".p")
+    return load("data/teams/teams_"+str(year)+".p")
 
 def saveAllTeams(teams):
-    dump("./data/teams/teams.p", teams)
+    dump("data/teams/teams.p", teams)
 
 def loadAllTeams():
-    return load("./data/teams/teams.p")
+    return load("data/teams/teams.p")
 
 def saveAllTeamsInfo(teams):
-    dump("./data/teams/teams_info.p", teams)
+    dump("data/teams_info.p", teams)
 
 def loadAllTeamsInfo():
-    try: return load("./data/teams/teams_info.p")
+    try: return load("data/teams_info.p")
     except Exception as e: return {}
